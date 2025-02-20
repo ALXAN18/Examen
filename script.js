740af828769e49f6d38605066ad5788273f50369
ALXAN18 <xanderdiaz29@hotmail.com>
2025-02-19 20:09:36 -0500

    script.js

--- script.js
+++ script.js
@@ -1,36 +1,22 @@
 let audio = document.getElementById('audio');
 let popupTemplate = document.getElementById('popup');
 let warning = document.getElementById('warning');
-let startButton = document.getElementById('startButton');
 
-startButton.addEventListener('click', () => {
-    audio.play();
-    startButton.style.display = 'none';
-});
+audio.play();
 
 setInterval(() => {
-    audio.play();
-}, 5000);
-
-setInterval(() => {
-    showPopup();
-}, 1000);
-
-function showPopup() {
     let newPopup = popupTemplate.cloneNode(true);
     newPopup.style.display = 'block';
 
-    let randomX = Math.random() * window.innerWidth - 200;
-    let randomY = Math.random() * window.innerHeight - 100;
-    newPopup.style.left = `${randomX}px`;
-    newPopup.style.top = `${randomY}px`;
+    newPopup.style.left = `${Math.random() * (window.innerWidth - 200)}px`;
+    newPopup.style.top = `${Math.random() * (window.innerHeight - 100)}px`;
 
     document.body.appendChild(newPopup);
 
     setTimeout(() => {
         newPopup.style.display = 'none';
     }, 2000);
-}
+}, 1000);
 
 setInterval(() => {
     warning.style.opacity = warning.style.opacity == 0 ? 1 : 0;
