02df2dc2040dc1a439a605b60b9d0e6191c52fd1
ALXAN18 <xanderdiaz29@hotmail.com>
2025-02-19 20:33:01 -0500

    script.js

--- script.js
+++ script.js
@@ -2,22 +2,34 @@ let audio = document.getElementById('audio');
 let popupTemplate = document.getElementById('popup');
 let warning = document.getElementById('warning');
 
-audio.play();
 
 setInterval(() => {
-    let newPopup = popupTemplate.cloneNode(true);
-    newPopup.style.display = 'block';
-
-    newPopup.style.left = `${Math.random() * (window.innerWidth - 200)}px`;
-    newPopup.style.top = `${Math.random() * (window.innerHeight - 100)}px`;
-
-    document.body.appendChild(newPopup);
-
-    setTimeout(() => {
-        newPopup.style.display = 'none';
-    }, 2000);
+    showPopup();
 }, 1000);
+
 
 setInterval(() => {
     warning.style.opacity = warning.style.opacity == 0 ? 1 : 0;
 }, 1000);
+
+
+document.addEventListener('click', () => {
+    audio.play();
+    showPopup();
+});
+
+function showPopup() {
+    let newPopup = popupTemplate.cloneNode(true);
+    newPopup.style.display = 'block';
+
+    let randomX = Math.random() * (window.innerWidth - 200);
+    let randomY = Math.random() * (window.innerHeight - 100);
+    newPopup.style.left = `${randomX}px`;
+    newPopup.style.top = `${randomY}px`;
+
+    document.body.appendChild(newPopup);
+
+    setTimeout(() => {
+        newPopup.remove();
+    }, 2000);
+}
